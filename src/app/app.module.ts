cff363333bc9d2202dfa6dc2444cc00b547dfa94
Olaniyi Awofisayo <olaniyiawofisayo@gmail.com>
2020-06-05 11:12:43 +0100

    material design added to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { ShareModule } from './share/share.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    ShareModule
+    ShareModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
