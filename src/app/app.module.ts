8267897f7c2b863a6211022e659ade0a53cb8854
Olaniyi Awofisayo <olaniyiawofisayo@gmail.com>
2020-06-10 13:02:55 +0100

    increase globe image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { CoreModule } from './core/core.module';
 import { ShareModule } from './share/share.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NotificationComponent } from './components/notification/notification.component';
+
 @NgModule({
-  declarations: [],
+  declarations: [ NotificationComponent ],
   imports: [ CommonModule ],
-  exports: [],
+  exports: [ NotificationComponent ],
   providers: [],
 })
 export class ShareModule {}
