5b53a59d5a3e5d6d921fe71519724ac683c270c6
Olaniyi Awofisayo <olaniyiawofisayo@gmail.com>
2020-06-14 19:25:03 +0100

    full page library added to project

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener, Inject } from '@angular/core';
+import { Component, OnInit, HostListener, Inject, Renderer2, ViewChild, ElementRef} from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
 @Component({
@@ -7,10 +7,36 @@ import { DOCUMENT } from '@angular/common';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('fullpageRef', {static: false}) fp_directive: ElementRef;
+  config;
+  fullpage_api;
 
   constructor(
-    @Inject(DOCUMENT) document
-  ) {}
+    @Inject(DOCUMENT) document,
+    private renderer: Renderer2
+  ) {
+    this.config = {
+      licenseKey: 'YOUR LICENSE KEY HERE',
+      anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+      menu: '#menu',
+      navigation: true,
+      verticalCentered: false,
+
+      // events callback
+      afterLoad: (origin, destination, direction) => {
+        // console.log(destination);
+      },
+      afterRender: () => {
+        // console.log('afterRender');
+      },
+      afterResize: (width, height) => {
+        // console.log('afterResize' + width + ' ' + height);
+      },
+      afterSlideLoad: (section, origin, destination, direction) => {
+        // console.log(destination);
+      }
+    };
+  }
 
   ngOnInit(): void { }
 
@@ -23,6 +49,39 @@ export class HomeComponent implements OnInit {
       const element = document.getElementById('navbar');
       element.classList.remove('sticky');
      }
+  }
+
+  getRef(fullPageRef) {
+    this.fullpage_api = fullPageRef;
+  }
+
+  addSection() {
+    // change background color
+    this.config['sectionsColor'] = Array(6).fill(0).map(x => this.randomColor());
+
+    // creating the section div
+    const section = this.renderer.createElement('div');
+    this.renderer.addClass(section, 'section');
+    this.renderer.setProperty(section, 'innerHTML', '<h3>New Section</h3>');
+    // adding section
+    this.renderer.appendChild(this.fp_directive.nativeElement, section);
+
+    this.fullpage_api.build();
+  }
+
+  removeLast() {
+    const lastSection = this.fp_directive.nativeElement.lastChild;
+
+    if (lastSection.isEqualNode(this.fullpage_api.getActiveSection().item)) {
+      this.fullpage_api.moveSectionUp();
+    }
+    lastSection.remove();
+
+    this.fullpage_api.build();
+  }
+
+  randomColor() {
+    return '#' + Math.random().toString(16).slice(-3);
   }
 }
 
